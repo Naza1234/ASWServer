0e820fd8e6e148425c705e5f2abea3b1cf249890
Ozurmba chibuike <ocbmf17@gmail.com>
2024-10-05 18:51:01 +0100

    agine

--- controllers/WithDrawalController.js
+++ controllers/WithDrawalController.js
@@ -1,18 +1,18 @@
 const WithDrawal = require('../models/WithDrawalModel');
 const User = require('../models/UserModel');
+const mongoose = require('mongoose');
 
-
-// Create a shipment package
+// Create a new withdrawal
 exports.createWithDrawal = async (req, res) => {
     try {
         const withDrawal = new WithDrawal(req.body);
-
         const savedWithDrawal = await withDrawal.save();
-        res.json(savedWithDrawal);
+        res.status(201).json(savedWithDrawal); // Send 201 status for created resource
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+
 // Get all withdrawals
 exports.getAllWithDrawals = async (req, res) => {
     try {
@@ -26,10 +26,7 @@ exports.getAllWithDrawals = async (req, res) => {
             
             const user = await User.findById(withDrawal.userId);
             
-            return {
-                user,
-                withDrawal
-            };
+            return { user, withDrawal };
         }));
         
         // Filter out any null values (entries with invalid or missing userId)
@@ -41,16 +38,16 @@ exports.getAllWithDrawals = async (req, res) => {
     }
 };
 
-// Find one withdrawal by id
+// Get one withdrawal by ID
 exports.findOneWithDrawalById = async (req, res) => {
     try {
         const withDrawal = await WithDrawal.findById(req.params.id);
+        if (!withDrawal) {
+            return res.status(404).json({ message: 'Withdrawal not found' });
+        }
+
         const user = await User.findById(withDrawal.userId);
-        
-        const data = {
-            withDrawal,
-            user
-        };
+        const data = { withDrawal, user };
 
         res.json(data);
     } catch (error) {
@@ -58,21 +55,26 @@ exports.findOneWithDrawalById = async (req, res) => {
     }
 };
 
-
-// Delete one shipment package by id
+// Delete one withdrawal by ID
 exports.deleteOneWithDrawalById = async (req, res) => {
     try {
-        await WithDrawal.findByIdAndDelete(req.params.id);
-        res.json({ message: 'Shipment package deleted successfully' });
+        const deletedWithDrawal = await WithDrawal.findByIdAndDelete(req.params.id);
+        if (!deletedWithDrawal) {
+            return res.status(404).json({ message: 'Withdrawal not found' });
+        }
+        res.json({ message: 'Withdrawal deleted successfully' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
 
-// Update one shipment package by id
+// Update one withdrawal by ID
 exports.updateOneWithDrawalById = async (req, res) => {
     try {
         const updatedWithDrawal = await WithDrawal.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedWithDrawal) {
+            return res.status(404).json({ message: 'Withdrawal not found' });
+        }
         res.json(updatedWithDrawal);
     } catch (error) {
         res.status(500).json({ message: error.message });
